bdda6dc1228fd7eb5f5a19a7253a4651e33bb004
Alex5X5 <alex.25.lehnert@gmail.com>
2026-01-09 13:42:18 +0100

    Initial Commit

---
+++ Logic/Models/Pokemons/Arkani.py
@@ -0,0 +1,16 @@
+from Logic.Models.Attacks.Punch import Punch
+from Logic.Models.Elements.Fire import Fire
+from Logic.Models.Elements.Water import Water
+from Logic.Models.Pokemons.Pokemon import Pokemon
+
+
+class Arkani(Pokemon):
+
+    def __init__(self, owner):
+        super().__init__(
+            Fire(),
+            Water(),
+            120,
+            100,
+            Punch(owner)
+        )
